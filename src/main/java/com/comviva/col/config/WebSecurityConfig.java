a796d8ec21c33afaa01c3092cce9a82be37ecca6
Samarth Shivanand Sangam <samarth.sangam@comviva.com>
2019-11-23 16:43:01 +0530

    Add Package WAR support, Add Actuator

--- src/main/java/com/comviva/col/config/WebSecurityConfig.java
+++ src/main/java/com/comviva/col/config/WebSecurityConfig.java
@@ -61,9 +61,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		// We don't need CSRF for this example
 		httpSecurity.csrf().disable()
 				// dont authenticate this particular request
-				.authorizeRequests().antMatchers("/authenticate", "/register", "/actuator/**").permitAll()
+				.authorizeRequests().antMatchers("/authenticate", "/register", "/actuator/**", "/manager/**")
+				.permitAll()
 				// all other requests need to be authenticated
-				.anyRequest().authenticated().and().
+				.antMatchers("/api/v1/**").authenticated().and().
 				// make sure we use stateless session; session won't be used to
 				// store user's state.
 				exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
